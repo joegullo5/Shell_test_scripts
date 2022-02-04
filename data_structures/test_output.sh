bf7677c07a6a833fe6cceb8659b8123f73803e1d
Joseph Gullo <jgullo@nd.edu>
2022-02-03 19:08:43 -0500

    Update

--- data_structures/test_output.sh
+++ data_structures/test_output.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+if [ $1 = "clean" ];then
+    rm arguments.txt
+    rm -r test
+    exit 1
+fi
 
-rm test/inputs/*txt
-rm test/solutions/*.out
+rm test/inputs/*txt > /dev/null 2>&1
+rm test/solutions/*.out > /dev/null 2>&1
 echo "Creating file structure"
-mkdir test
-mkdir test/inputs test/solutions test/your_outputs
+mkdir test > /dev/null 2>&1
+mkdir test/inputs test/solutions test/your_outputs > /dev/null 2>&1
 echo "Getting test files"
 wget -q https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC02/test1.txt --directory-prefix=test/inputs
 wget -q https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC02/test2.txt --directory-prefix=test/inputs
@@ -52,5 +57,3 @@ do
         echo "Differences found between your ${line}_${line1}gen.out and the soluctions"
     fi
 done
-
-
